b2efd32e57b25931529fb46febd71345b18fee21
Brian Adams <bpeteradams@gmail.com>
2021-06-05 14:57:16 -0400

    finished up static serving of html files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,4 +9,12 @@ router.get('/exercise', async (req, res) => {
   res.sendFile(path.join(__dirname, '../public/exercise.html'));
 });
 
+router.get('/stats', async (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
+router.get('*', async (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 module.exports = router;
