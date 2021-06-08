e04bde821a9b0c5df4e32d1167efdf403ec4a0b7
Brian Adams <bpeteradams@gmail.com>
2021-06-08 16:12:54 -0400

    wrote route to add a new workoutA

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 const Workout = require('../models/Workout');
 
 router.get('/api/workouts', async (req, res) => {
-   try {
-     const workouts = await Workout.aggregate([
-       { $addFields: {
-         totalDuration: { $sum: '$exercises.duration' }
-       }}
-     ])
-     res.status(200).json(workouts);
-   } catch (error) {
-     res.status(500).json(error);
-   }
+  try {
+    const workouts = await Workout.aggregate([
+      { $addFields: {
+        totalDuration: { $sum: '$exercises.duration' }
+      }}
+    ])
+    res.status(200).json(workouts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.put('/api/workouts/:id', async (req, res) => {
@@ -30,4 +30,12 @@ router.put('/api/workouts/:id', async (req, res) => {
   }
 });
 
+router.post('/api/workouts/', async (req, res) => {
+  try {
+    const newWorkout = await Workout.create(req.body);
+    res.status(200).json(newWorkout)
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 module.exports = router;
