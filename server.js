4303698acbf751a5356f03511eb75c11d810b576
Brian Adams <bpeteradams@gmail.com>
2021-06-08 17:49:01 -0400

    fixed env var

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 app.use(require('./controllers/apiRoutes'));
 app.use(require('./controllers/homeRoutes'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGOODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
