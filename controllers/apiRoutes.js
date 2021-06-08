359b2031d6ad5252fd2a13d7cd01f4d5f7adf0d0
Brian Adams <bpeteradams@gmail.com>
2021-06-08 16:19:03 -0400

    wrote a route to show dashboard info in /api/workouts/range

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -4,10 +4,29 @@ const Workout = require('../models/Workout');
 router.get('/api/workouts', async (req, res) => {
   try {
     const workouts = await Workout.aggregate([
-      { $addFields: {
-        totalDuration: { $sum: '$exercises.duration' }
-      }}
+      {
+        $addFields: {
+          totalDuration: { $sum: '$exercises.duration' },
+        },
+      },
+    ]);
+    res.status(200).json(workouts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get('/api/workouts/range', async (req, res) => {
+  try {
+    const workouts = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: '$exercises.duration' },
+        },
+      },
     ])
+      .limit(7)
+      .sort({ day: 1 });
     res.status(200).json(workouts);
   } catch (error) {
     res.status(500).json(error);
@@ -33,7 +52,7 @@ router.put('/api/workouts/:id', async (req, res) => {
 router.post('/api/workouts/', async (req, res) => {
   try {
     const newWorkout = await Workout.create(req.body);
-    res.status(200).json(newWorkout)
+    res.status(200).json(newWorkout);
   } catch (error) {
     res.status(500).json(error);
   }
